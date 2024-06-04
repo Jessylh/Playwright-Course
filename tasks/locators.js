23fb1fd3b86f8de3b1b71b2263e6e01116b2c5cd
Jessylh <jlhg.96@gmail.com>
2024-06-03 21:12:34 -0600

    Task

---
+++ tasks/locators.js
@@ -0,0 +1,13 @@
+/*
+https://thinking-tester-contact-list.herokuapp.com/
+jogeh79883@acuxi.com
+jogeh79883
+ */
+
+//Login
+document.querySelector("#email").value = "jogeh79883@acuxi.com"
+document.querySelector("#password").value = "jogeh79883"
+document.querySelector("#submit").click()
+
+//assertion
+document.querySelector("#logout").click()
